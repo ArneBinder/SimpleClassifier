ec60544120f45a33c2ab3842e7ac7dcdd7a3fccf
blackgreetz <blackgreetz@googlemail.com>
2014-01-24 14:51:42 +0100

    Added serial version UIDs to remove warnings

--- src/Classifier/bean/Exceptions.java
+++ src/Classifier/bean/Exceptions.java
@@ -4,40 +4,52 @@ package Classifier.bean;
  * Created by Arne on 22.01.14.
  */
 public class Exceptions {
-	public static class SRLException extends Exception {
-		SRLException(String message){
-			super(message);
-		}
-	}
+    public static class SRLException extends Exception {
+	private static final long serialVersionUID = -8114236520060873531L;
 
-	public static class IDrefNotInSentenceException extends SRLException {
-		IDrefNotInSentenceException(String message){
-			super(message);
-		}
+	SRLException(String message) {
+	    super(message);
 	}
+    }
 
-	public static class FeatureTypeNotFoundException extends SRLException {
-		FeatureTypeNotFoundException(String message){
-			super(message);
-		}
-	}
+    public static class IDrefNotInSentenceException extends SRLException {
+	private static final long serialVersionUID = 7278986165517473377L;
 
-	public static class RootNotInPathException extends SRLException {
-		RootNotInPathException(String message){
-			super(message);
-		}
+	IDrefNotInSentenceException(String message) {
+	    super(message);
 	}
+    }
 
-	public static class FeatureValueNotCalculatedException extends SRLException {
-		public FeatureValueNotCalculatedException(String message){
-			super(message);
-		}
-	}
+    public static class FeatureTypeNotFoundException extends SRLException {
+	private static final long serialVersionUID = 8542858231316697802L;
 
-	public static class NoTargetsException extends SRLException {
-		NoTargetsException(String message){
-			super(message);
-		}
+	FeatureTypeNotFoundException(String message) {
+	    super(message);
 	}
+    }
+
+    public static class RootNotInPathException extends SRLException {
+	private static final long serialVersionUID = -2633669124549081061L;
+
+	RootNotInPathException(String message) {
+	    super(message);
+	}
+    }
+
+    public static class FeatureValueNotCalculatedException extends SRLException {
+	private static final long serialVersionUID = -4119063161178541984L;
+
+	public FeatureValueNotCalculatedException(String message) {
+	    super(message);
+	}
+    }
+
+    public static class NoTargetsException extends SRLException {
+	private static final long serialVersionUID = -7578337315534309826L;
+
+	NoTargetsException(String message) {
+	    super(message);
+	}
+    }
 
 }
